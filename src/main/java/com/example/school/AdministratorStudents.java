b664ad7fb0737f80f494c0f89d1ddf9879f50001
koporok <122740702+koporok@users.noreply.github.com>
2023-12-14 17:52:11 +0300

    Работа с окном администратора_ученик

--- src/main/java/com/example/school/AdministratorStudents.java
+++ src/main/java/com/example/school/AdministratorStudents.java
@@ -1,4 +1,116 @@
 package com.example.school;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class AdministratorStudents {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private AnchorPane Student;
+
+    @FXML
+    private TextField StudentDate;
+
+    @FXML
+    private TextField StudentFIO;
+
+    @FXML
+    private TableView<?> StudentTable;
+
+    @FXML
+    private TableColumn<?, ?> StudentTableDate;
+
+    @FXML
+    private TableColumn<?, ?> StudentTableFIO;
+
+    @FXML
+    private TableColumn<?, ?> StudentTableGroup;
+
+    @FXML
+    private TableColumn<?, ?> StudentTableId;
+
+    @FXML
+    private TableColumn<?, ?> StudentTableLogin;
+
+    @FXML
+    private TableColumn<?, ?> StudentTableTelephone;
+
+    @FXML
+    private TableColumn<?, ?> StudentTableYear;
+
+    @FXML
+    private TextField StudentTelephone;
+
+    @FXML
+    private TextField StudentYear;
+
+    @FXML
+    private Tab User;
+
+    @FXML
+    private TextField UserDate;
+
+    @FXML
+    private TextField UserFIO;
+
+    @FXML
+    private TextField UserLogin;
+
+    @FXML
+    private TableView<?> UserTable;
+
+    @FXML
+    private TableColumn<?, ?> UserTableDate;
+
+    @FXML
+    private TableColumn<?, ?> UserTableFIO;
+
+    @FXML
+    private TableColumn<?, ?> UserTableId;
+
+    @FXML
+    private TableColumn<?, ?> UserTableTelephone;
+
+    @FXML
+    private TableColumn<?, ?> UserTableYear;
+
+    @FXML
+    private TextField UserTelephone;
+
+    @FXML
+    private TextField UserYear;
+
+    @FXML
+    private AnchorPane addUser;
+
+    @FXML
+    private Button createLogin;
+
+    @FXML
+    private Button deleteStudent;
+
+    @FXML
+    private Button deleteUser;
+
+    @FXML
+    private Button editStudent;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
